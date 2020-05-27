40190043a3cbeeeb64652a723303fef0ea35ac27
Kyle Parisi <kyle@saytechnologies.com>
2020-05-27 09:59:04 -0400

    Initial Commit

---
+++ Swifty/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Swifty
+//
+//  Created by Kyle Parisi on 5/27/20.
+//  Copyright © 2020 Kyle Parisi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

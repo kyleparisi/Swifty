98c97bd307448dc51b97d3455128fb5a86ce0fd3
Kyle Parisi <kyleparisi@gmail.com>
2020-12-28 08:11:23 -0500

    document based app

---
+++ Swifty/Content.swift
@@ -0,0 +1,23 @@
+import Foundation
+import Cocoa
+
+class Content: NSObject {
+    @objc dynamic var contentString = ""
+    
+    public init(contentString: String) {
+        self.contentString = contentString
+    }
+    
+}
+
+extension Content {
+    
+    func read(from data: Data) {
+        contentString = String(bytes: data, encoding: .utf8)!
+    }
+    
+    func data() -> Data? {
+        return contentString.data(using: .utf8)
+    }
+    
+}
--- Swifty/MyViewController.swift
+++ Swifty/MyViewController.swift
@@ -84,8 +84,35 @@ var LANGUAGE = "js"
 
 var insertionLocations: Set<Int> = []
 
-class MyViewContoller: NSViewController {
-    @IBOutlet var textView: MyTextView?
+class MyViewContoller: NSViewController, NSTextViewDelegate {
+//    @IBOutlet var textView: MyTextView?
+    
+    /// - Tag: setRepresentedObjectExample
+    override var representedObject: Any? {
+        didSet {
+            // Pass down the represented object to all of the child view controllers.
+            for child in children {
+                child.representedObject = representedObject
+            }
+        }
+    }
+    
+    weak var document: Document? {
+        if let docRepresentedObject = representedObject as? Document {
+            return docRepresentedObject
+        }
+        return nil
+    }
+    
+    // MARK: - NSTextViewDelegate
+
+    func textDidBeginEditing(_ notification: Notification) {
+        document?.objectDidBeginEditing(self)
+    }
+
+    func textDidEndEditing(_ notification: Notification) {
+        document?.objectDidEndEditing(self)
+    }
 }
 
 class MyTextStorage: NSTextStorage {
